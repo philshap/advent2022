14b8cad6c8ffb17bc15dbda1c9a16bb48483c34e
Phil Shapiro <philshap@gmail.com>
2022-12-04 10:04:39 -0500

    use re-seq instead of split

--- src/day4.clj
+++ src/day4.clj
@@ -1,15 +1,11 @@
-(ns day4
-  (:require
-    [clojure.string :as str]))
+(ns day4)
 
-;; 2-4,6-8
+;; 2-4,6-8... => [2 4 6 8]...
 (def input
-  (-> "src/day4-input.txt"
-      slurp
-      (str/split #"[\-,\n]")
-      (->>
-        (map parse-long)
-        (partition 4))))
+  (->> (slurp "src/day4-input.txt")
+       (re-seq #"\d+")
+       (map parse-long)
+       (partition 4)))
 
 (defn subset? [[low1 hi1 low2 hi2]]
   (or (<= low1 low2 hi2 hi1)
@@ -23,6 +19,7 @@
 (defn overlap? [[low1 hi1 low2 hi2]]
   (or (<= low1 low2 hi1) (<= low1 hi2 hi1)
       (<= low2 low1 hi2) (<= low2 hi1 hi2)))
+
 (defn part2 []
   (->> input
        (filter overlap?)
