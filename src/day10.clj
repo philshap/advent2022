29cbe3b661f20a5c42667a9396d4b499299f0138
Phil Shapiro <philshap@gmail.com>
2022-12-10 09:45:39 -0500

    day 10

---
+++ src/day10.clj
@@ -0,0 +1,45 @@
+(ns day10
+  (:require [clojure.string :as str]))
+
+(def input
+  (-> "src/day10-input.txt"
+      slurp
+      str/split-lines))
+
+(defn run-one [[[command & rest :as commands] x-reg carry]]
+  (if (nil? carry)
+    [rest x-reg (-> (str/split command #" ") last parse-long)]
+    [commands (+ x-reg carry) nil]))
+
+(defn signal [cycles regs]
+  (map #(* % (nth regs (dec %))) cycles))
+
+(defn part1 []
+  (->> (iterate run-one [input 1 nil])
+       (map second)
+       (signal [20 60 100 140 180 220])
+       (reduce +)))
+
+(def width 40)
+(def lines 6)
+
+(defn to-pixel [[pos x-reg]]
+  (let [pos (mod pos width)]
+    (if (<= (dec pos) x-reg (inc pos))
+      \#
+      \.)))
+
+(defn part2 []
+  (->> (iterate run-one [input 1 nil])
+       (take (* width lines))
+       (map second)
+       (map-indexed vector)
+       (map to-pixel)
+       (partition width)))
+
+; part 1:  14620
+; part 2:  BJFRHRFU
+(comment
+  (println "part 1: " (part1))
+  (println "part 2: " (part2))
+  )
