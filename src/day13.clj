b0c3d18573408522bdd5d8b00860893d495d912c
Phil Shapiro <philshap@gmail.com>
2022-12-13 05:51:48 -0500

    day 13

---
+++ src/day13.clj
@@ -0,0 +1,54 @@
+(ns day13
+  (:require
+    [clojure.edn :as edn]
+    [clojure.string :as str]))
+
+(def input
+  (-> "src/day13-input.txt"
+      slurp
+      (str/split #"\n\n")
+      (->> (map #(map edn/read-string (str/split % #"\n"))))))
+
+(defn ordered? [left right]
+  (cond
+    (and (number? left) (number? right)) (if (= left right) :unknown (< left right))
+    (and (vector? left) (vector? right))
+    (loop [left left
+           right right]
+      (cond
+        (empty? left) (if (empty? right) :unknown true)
+        (empty? right) false
+        :else (let [inner (ordered? (first left) (first right))]
+                (if (= inner :unknown)
+                  (recur (rest left) (rest right))
+                  inner))))
+    (number? left) (recur [left] right)
+    (number? right) (recur left [right])))
+
+(defn part1 []
+  (->> input
+       (keep-indexed
+         (fn [index [left right]]
+           (if (ordered? left right)
+             (inc index))))
+       (reduce +)))
+
+(def div-packets [[[2]] [[6]]])
+(defn part2 []
+  (->> input
+       (reduce concat)
+       (concat div-packets)
+       (sort ordered?)
+       (keep-indexed
+         (fn [index packet]
+           (if (some #(= % packet) div-packets)
+             (inc index))))
+       (reduce *)))
+
+; part 1:  5390
+; part 2:  19261
+
+(comment
+  (println "part 1: " (part1))
+  (println "part 2: " (part2))
+  )
